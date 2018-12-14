121b2a0418462e70c4d0a6ef611c42032847d4cb
OraneG20 <44150109+OraneG20@users.noreply.github.com>
2018-12-13 20:32:16 -0500

    Getting user input

--- project3.s
+++ project3.s
@@ -3,3 +3,9 @@
         user_input: .space 9000
         input_empty: .asciiz "Input is empty."
         wrong_base: .asciiz "Invalid base-35 number."  
+.text
+main:
+        # getting user input
+
+        li $v0, 8
+        la $a0, user_input
