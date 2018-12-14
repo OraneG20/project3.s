24f06a178bb0b511773d6673d93be4c1c653e2a1
OraneG20 <44150109+OraneG20@users.noreply.github.com>
2018-12-13 20:30:56 -0500

    Setting up data

---
+++ project3.s
@@ -0,0 +1,5 @@
+.data
+        input_too_long: .asciiz "Input is too long."
+        user_input: .space 9000
+        input_empty: .asciiz "Input is empty."
+        wrong_base: .asciiz "Invalid base-35 number."  
